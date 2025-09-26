3d417ad5e6ff66751d33ac7bd8acb7ff43b66b2e
Artem <rabogoshvili_art@inbox.ru>
2025-09-26 17:28:28 +0500

    search-bar css modified

--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -41,27 +41,30 @@ const SearchBar = () => {
     return (
         <Col lg="12">
             <div className="search__bar">
-                <Form className='d-flex align-items-center gap-4'>
-
+                <Form className='form d-flex align-items-center gap-4'>
                     <FormGroup className='d-flex gap-3 form__group form__group-last'>
                         <span><i class="ri-map-pin-line"></i></span>
-                        <div><h6>Location</h6>
-                            <input className="search__bar_input" type="text" placeholder='Where are you going?' ref={locationRef} /></div>
+                        <div>
+                            <h6>Location</h6>
+                            <input className="search__bar_input" type="text" placeholder='Where are you going?' ref={locationRef} />
+                        </div>
                     </FormGroup>
 
                     <FormGroup className='d-flex gap-3 form__group form__group-last'>
                         <span><i class="ri-map-pin-time-line"></i></span>
-                        <div><h6>Distance k/m</h6>
-                            <input type="number" placeholder='Where are you going?' ref={distanceRef} /></div>
+                        <div>
+                            <h6>Distance k/m</h6>
+                            <input type="number" placeholder='Distance in km' ref={distanceRef} />
+                        </div>
                     </FormGroup>
-
 
                     <FormGroup className='d-flex gap-3 form__group'>
                         <span><i class="ri-group-line"></i></span>
-                        <div><h6>Max People</h6>
-                            <input type="number" placeholder='0' ref={maxGroupSizeRef} /></div>
+                        <div>
+                            <h6>Max People</h6>
+                            <input type="number" placeholder='0' ref={maxGroupSizeRef} />
+                        </div>
                     </FormGroup>
-
 
                     <span className="search__icon" type="submit" onClick={searchHandler}>
                         <i class="ri-search-line"></i>
