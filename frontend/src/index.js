0bb9136d45c131091352234c1056ace11b991d55
Artem <rabogoshvili_art@inbox.ru>
2025-09-29 15:00:40 +0500

    redirect for www. added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
-
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+if (window.location.hostname === "www.samovar-tours.ru") {
+  window.location.href = window.location.href.replace(
+    "www.samovar-tours.ru",
+    "samovar-tours.ru"
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
